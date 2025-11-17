204eca68a14fe4112bed0c22e5b06174b9336205
Josias Macillon <macillonjosias59@gmail.com>
2025-11-17 12:02:22 -0500

    mon premier commit

--- app/src/main/java/com/example/flashcardapp/MainActivity.kt
+++ app/src/main/java/com/example/flashcardapp/MainActivity.kt
@@ -3,16 +3,36 @@ package com.example.flashcardapp
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         val flashcardQuestion = findViewById<TextView>(R.id.flashcard_question)
         val flashcardAnswer = findViewById<TextView>(R.id.flashcard_answer)
+        val flashcardAnswer1 = findViewById<TextView>(R.id.flashcard_answer_1)
+        val flashcard_answer_2 = findViewById<TextView>(R.id.flashcard_answer_2)
+        val flashcard_answer_3 = findViewById<TextView>(R.id.flashcard_answer_3)
+        val flashcardAnswer4 = findViewById<TextView>(R.id.flashcard_answer_4)
 
+        flashcardAnswer4.setOnClickListener {
+            flashcardAnswer4.setBackgroundColor(ContextCompat.getColor(this, R.color.red))
+        }
+        flashcard_answer_3.setOnClickListener {
+            flashcard_answer_3.setBackgroundColor(ContextCompat.getColor(this, R.color.red))
+        }
+
+        flashcard_answer_2.setOnClickListener {
+            flashcard_answer_2.setBackgroundColor(ContextCompat.getColor(this, R.color.green))
+        }
+        flashcardAnswer1.setOnClickListener {
+            flashcardAnswer1.setBackgroundColor(ContextCompat.getColor(this, R.color.red))
+        }
         flashcardQuestion.setOnClickListener {
             flashcardQuestion.visibility = View.INVISIBLE
             flashcardAnswer.visibility = View.VISIBLE
@@ -23,4 +43,6 @@ class MainActivity : AppCompatActivity() {
             flashcardAnswer.visibility = View.INVISIBLE
             }
         }
+
+
 }
